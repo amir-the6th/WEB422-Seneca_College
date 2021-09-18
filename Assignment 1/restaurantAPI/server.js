fc50fef8210b0faf479188fef64906bfd0ba5143
asabagh <asabagh@myseneca.ca>
2021-09-17 21:02:38 -0400

    Update server.js

--- Assignment 1/restaurantAPI/server.js
+++ Assignment 1/restaurantAPI/server.js
@@ -7,7 +7,7 @@ const HTTP_PORT = process.env.PORT || 6060;
 
 // Add support for incoming JSON entities
 app.use(express.json());
-
+// Add support for CORS
 app.use(cors());
 
 // Deliver the app's home page to browser clients
@@ -15,7 +15,29 @@ app.get('/', (req, res) => {
   res.json({ message: 'API Listening' });
 });
 
-// Tell the app to start listening for requests
-app.listen(HTTP_PORT, () => {
-  console.log('Ready to handle requests on port ' + HTTP_PORT);
+app.get('/api/restaurants', (req, res) => {
+  // MUST return HTTP 201
+  res.status(201).json(RestaurantDB.addNewRestaurant(req.body));
 });
+
+app.post('/api/restaurants', (req, res) => {
+  // MUST return HTTP 201
+  res.status(201).json(RestaurantDB.addNewRestaurant(req.body));
+});
+
+// Tell the app to start listening for requests
+// app.listen(HTTP_PORT, () => {
+//   console.log('Ready to handle requests on port ' + HTTP_PORT);
+// });
+
+db.initialize(
+  'mongodb+srv://ass6User:AmirHS1380@cluster0.wbukp.mongodb.net/sample_restaurants?retryWrites=true&w=majority'
+)
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log(`server listening on: ${HTTP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
