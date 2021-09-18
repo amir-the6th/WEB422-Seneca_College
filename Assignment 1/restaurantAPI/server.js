71f52acbb147cbeb3284eba6cc007049c2038aee
asabagh <asabagh@myseneca.ca>
2021-09-17 23:11:41 -0400

    Update signature

--- Assignment 1/restaurantAPI/server.js
+++ Assignment 1/restaurantAPI/server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************************
+ * WEB422 – Assignment 1
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: Amirhossein Sabagh     Student ID: 152956199     Date: 2021-08-17
+ * Heroku Link: https://enigmatic-forest-52710.herokuapp.com
+ *
+ * ******************************************************************************************/
 const express = require('express');
 const cors = require('cors');
 const RestaurantDB = require('./modules/restaurantDB.js');
