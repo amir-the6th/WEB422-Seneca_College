51767debbe151bcb8554260c85b21c812d814ab8
asabagh <asabagh@myseneca.ca>
2021-10-01 20:22:24 -0400

    Add query validation

--- Assignment 1/restaurantAPI/server.js
+++ Assignment 1/restaurantAPI/server.js
@@ -13,55 +13,94 @@ const cors = require('cors');
 const RestaurantDB = require('./modules/restaurantDB.js');
 const db = new RestaurantDB();
 const app = express();
+const path = require('path');
 const HTTP_PORT = process.env.PORT || 6060;
-const dotenv = require('dotenv').config();
-const mongoLogin = process.env.MONGODB_CONN_STRING;
+require('dotenv').config();
+// query validation
+const { query, validationResult } = require('express-validator');
 
 // Add support for incoming JSON entities
 app.use(express.json());
 // Add support for CORS
 app.use(cors());
 
+const mongoLogin = process.env.MONGODB_CONN_STRING;
+// If we don't have it, crash immediately
+if (!mongoLogin) {
+  console.error('missing environment variable MONGODB_CONN_STRING');
+  process.exit(1);
+}
+
 // Deliver the app's home page to browser clients
 app.get('/', (req, res) => {
-  res.json({ message: 'API Listening' });
+  //res.json({ message: 'API Listening' });
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+/* This route uses the body of the request to add a new "Restaurant" 
+ document to the collection */
+app.post('/api/restaurants', (req, res) => {
+  // MUST return HTTP 201
+  db.addNewRestaurant(req.body)
+    .then((restaurant) => {
+      res.status(201).json({
+        _id: restaurant._id,
+        message: 'Restaurant added successfully!',
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `Could not add the restaurant! Error: ${err}`,
+      });
+    });
 });
 
 /* This route returns all "Restaurant" objects for a specific "page" to
-the client as well as optionally filtering by "borough", if provided. */
-app.get('/api/restaurants', (req, res) => {
-  let page = req.query.page;
-  let perPage = req.query.perPage;
-  let borough = req.query.borough;
-  db.getAllRestaurants(page, perPage, borough).then((data) => {
-    if (data.length != 0) {
-      res.status(200).json(data);
-    } else {
-      res.status(404).json({
-        message: 'No restaurant was found!',
-      });
+ the client as well as optionally filtering by "borough", if provided. */
+app.get(
+  '/api/restaurants',
+  [
+    query('page')
+      .isInt({ min: 1 })
+      .withMessage('page param must be the whole number 1 or greater.'),
+    query('perPage')
+      .isInt({ min: 1 })
+      .withMessage('perPage param must be the whole number 1 or greater.'),
+    query('borough').isString().optional(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  });
-});
+    // let page = req.query.page;
+    // let perPage = req.query.perPage;
+    // let borough = req.query.borough;
+    const { page, perPage, borough } = req.query;
+    db.getAllRestaurants(page, perPage, borough)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: `No restaurant was found! Error: ${err}`,
+        });
+      });
+  }
+);
 
 /* This route must accept a numeric route parameter that represents the 
-_id of the desired restaurant object to return a specific "Restaurant"
-object to the client. */
+ _id of the desired restaurant object to return a specific "Restaurant"
+ object to the client. */
 app.get('/api/restaurants/:id', (req, res) => {
   let id = req.params.id;
   db.getRestaurantById(id)
     .then((data) => {
-      if (data) {
-        res.status(200).json(data);
-      } else {
-        res.status(404).json({
-          message: 'No match was found!',
-        });
-      }
+      res.status(200).json(data);
     })
     .catch((err) => {
-      res.status(400).json({
-        message: err,
+      res.status(500).json({
+        message: `No match was found! Error: ${err}`,
       });
     });
 });
@@ -76,8 +115,8 @@ app.put('/api/restaurants/:id', (req, res) => {
       });
     })
     .catch((err) => {
-      res.status(400).json({
-        message: err,
+      res.status(500).json({
+        message: `an error occurred: ${err}`,
       });
     });
 });
@@ -87,30 +126,14 @@ app.delete('/api/restaurants/:id', (req, res) => {
   let id = req.params.id;
   db.deleteRestaurantById(id)
     .then(() => {
-      res.status(204).json({
-        message: `The restaurant with id ${id} deleted successfully!`,
-      });
-    })
-    .catch((err) => {
-      res.status(400).json({
-        message: err,
-      });
-    });
-});
-
-/* This route uses the body of the request to add a new "Restaurant" 
-document to the collection */
-app.post('/api/restaurants', (req, res) => {
-  // MUST return HTTP 201
-  db.addNewRestaurant(req.body)
-    .then(() => {
-      res.status(201).json({
-        message: 'Restaurant added successfully!',
-      });
+      // res.status(204).json({
+      //   message: `The restaurant with id ${id} deleted successfully!`,
+      // });
+      res.status(204).end();
     })
     .catch((err) => {
       res.status(500).json({
-        message: 'Could not add the restaurant!',
+        message: `an error occurred: ${err}`,
       });
     });
 });
@@ -124,5 +147,5 @@ db.initialize(mongoLogin)
     });
   })
   .catch((err) => {
-    console.log(err);
+    console.error(err);
   });
