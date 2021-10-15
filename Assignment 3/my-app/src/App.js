772740776c45c40e793df990773a5fc923b7b123
asabagh <asabagh@myseneca.ca>
2021-10-14 22:54:58 -0400

    Set up the navabar and define handleSubmit function

--- Assignment 3/my-app/src/App.js
+++ Assignment 3/my-app/src/App.js
@@ -9,11 +9,52 @@ import {
   Navbar,
   Row,
 } from 'react-bootstrap';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
-  return null;
+  const [searchString, setSearchString] = '';
+  let history = useHistory();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    history.push(`/restaurants?borough=${searchString}`);
+    setSearchString('');
+  }
+
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <LinkContainer to="/">
+          <Navbar.Brand>New York Restaurants</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to="/restaurants">
+              <Nav.Link>Full List</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link>About</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <Form onSubmit={handleSubmit} inline>
+            <FormControl
+              type="text"
+              placeholder="Borough"
+              className="mr-sm-2"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
+            />
+            <Button type="submit" variant="outline-success">
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <br />
+    </>
+  );
 }
 
 export default App;
