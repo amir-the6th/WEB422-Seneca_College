24094b2740411062bdc4bd7a9c8994a87cbf85fb
asabagh <asabagh@myseneca.ca>
2021-10-15 23:45:53 -0400

    Implement NotFound component

---
+++ Assignment 3/my-app/src/NotFound.js
@@ -0,0 +1,14 @@
+// For now. a default NotFound
+export default function NotFound() {
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <h3 className="text-center">
+        <strong>404: </strong>Not Found
+      </h3>
+      <p className="text-center">Nothing here, I'm afraid...</p>
+    </>
+  );
+}
