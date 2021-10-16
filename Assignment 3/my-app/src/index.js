af65d8d50a8231acedda701cb8fcbd38afde84f3
asabagh <asabagh@myseneca.ca>
2021-10-15 23:41:02 -0400

    Add signature

--- Assignment 3/my-app/src/index.js
+++ Assignment 3/my-app/src/index.js
@@ -1,3 +1,12 @@
+/*********************************************************************************************
+ * WEB422 – Assignment 3
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: Amirhossein Sabagh     Student ID: 152956199     Date: 2021-10-15
+ *
+ * ******************************************************************************************/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
