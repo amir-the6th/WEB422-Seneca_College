1a143500922dc5a161c9b006a7db9646b06365eb
asabagh <asabagh@myseneca.ca>
2021-11-20 20:12:53 -0500

    Update AlbumComponent to accommodate retrieve live data

--- Assignment 5/web422-a5/src/app/album/album.component.ts
+++ Assignment 5/web422-a5/src/app/album/album.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MusicDataService } from '../music-data.service';
 import albumData from '../data/SearchResultsAlbum.json';
 
 @Component({
@@ -7,11 +11,32 @@ import albumData from '../data/SearchResultsAlbum.json';
   styleUrls: ['./album.component.css'],
 })
 export class AlbumComponent implements OnInit {
-  album: any;
+  album: Array<any> | undefined;
+  private albumSubscribe: Subscription | undefined;
+  private id: any;
 
-  constructor() {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute,
+    private musicDataService: MusicDataService
+  ) {}
 
   ngOnInit(): void {
-    this.album = albumData;
+    this.id = this.route.snapshot.params['id'];
+    this.albumSubscribe = this.musicDataService
+      .getAlbumById(this.id)
+      .subscribe((data) => {
+        return (this.album = data);
+      });
+  }
+
+  addToFavourites(trackID: any) {
+    if (this.musicDataService.addToFavourites(trackID)) {
+      this.snackBar.open('Adding to Favourites...', 'Done', { duration: 1500 });
+    }
+  }
+
+  ngOnDestroy() {
+    this.albumSubscribe?.unsubscribe();
   }
 }
