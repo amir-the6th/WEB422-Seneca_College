775ea5e02f4a5fe3caf33b2e57052d10a56dff14
asabagh <asabagh@myseneca.ca>
2021-11-20 19:32:47 -0500

    Update ArtistDiscographyComponent to accommodate retrieve live data

--- Assignment 5/web422-a5/src/app/artist-discography/artist-discography.component.ts
+++ Assignment 5/web422-a5/src/app/artist-discography/artist-discography.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import albumData from '../data/SearchResultsAlbums.json';
 import artistData from '../data/SearchResultsArtist.json';
+import { MusicDataService } from '../music-data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-artist-discography',
@@ -8,18 +11,41 @@ import artistData from '../data/SearchResultsArtist.json';
   styleUrls: ['./artist-discography.component.css'],
 })
 export class ArtistDiscographyComponent implements OnInit {
-  albums: any;
-  artist: any;
-  constructor() {}
+  albums: Array<any> | undefined;
+  artist: Array<any> | undefined;
+  private albumsSubscribe: Subscription | undefined;
+  private artistSubscribe: Subscription | undefined;
+  private id: any;
+
+  constructor(
+    private musicDataService: MusicDataService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.artistSubscribe = this.musicDataService
+      .getArtistById(this.id)
+      .subscribe((data) => {
+        return (this.artist = data);
+      });
+
+    this.albumsSubscribe = this.musicDataService
+      .getAlbumsByArtistId(this.id)
+      .subscribe((data) => {
+        return (this.albums = data.items.filter(
+          (curValue: any, index: any, self: any) =>
+            self.findIndex(
+              (t: any) => t.name.toUpperCase() === curValue.name.toUpperCase()
+            ) === index
+        ));
+      });
+
     this.albums = albumData.items.filter(
       (curValue: any, index: any, self: any) =>
         self.findIndex(
           (t: any) => t.name.toUpperCase() === curValue.name.toUpperCase()
         ) === index
     );
-
-    this.artist = artistData;
   }
 }
