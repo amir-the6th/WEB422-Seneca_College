2acd9017920ed355abf1450869e64b0dcb74c17a
asabagh <asabagh@myseneca.ca>
2021-11-21 23:44:33 -0500

    Add handleSearch() method to analyze the query passed from search box

--- Assignment 5/web422-a5/src/app/app.component.ts
+++ Assignment 5/web422-a5/src/app/app.component.ts
@@ -1,13 +1,14 @@
 /*********************************************************************************************
- * WEB422 – Assignment 4
+ * WEB422 – Assignment 5
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students.
  *
- * Name: Amirhossein Sabagh     Student ID: 152956199     Date: 2021-11-05
+ * Name: Amirhossein Sabagh     Student ID: 152956199     Date: 2021-11-21
  *
  * ******************************************************************************************/
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -15,5 +16,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'web422-a4';
+  title = 'web422-a5';
+  searchString: string | undefined;
+
+  constructor(private router: Router) {}
+
+  handleSearch() {
+    this.router.navigate(['/search'], {
+      queryParams: { q: this.searchString },
+    });
+    this.searchString = '';
+  }
 }
