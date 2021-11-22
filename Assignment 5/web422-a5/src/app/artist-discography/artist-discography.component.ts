02c25dc13b8729fd2dafa3802e829f68e1dacff1
asabagh <asabagh@myseneca.ca>
2021-11-21 23:45:18 -0500

    Fix property types - remove dependency to static data

--- Assignment 5/web422-a5/src/app/artist-discography/artist-discography.component.ts
+++ Assignment 5/web422-a5/src/app/artist-discography/artist-discography.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import albumData from '../data/SearchResultsAlbums.json';
-import artistData from '../data/SearchResultsArtist.json';
 import { MusicDataService } from '../music-data.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -11,10 +9,12 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./artist-discography.component.css'],
 })
 export class ArtistDiscographyComponent implements OnInit {
-  albums: Array<any> | undefined;
-  artist: Array<any> | undefined;
+  // albums: Array<any> | undefined;
+  // artist: Array<any> | undefined;
   private albumsSubscribe: Subscription | undefined;
   private artistSubscribe: Subscription | undefined;
+  albums: any;
+  artist: any;
   private id: any;
 
   constructor(
@@ -40,13 +40,6 @@ export class ArtistDiscographyComponent implements OnInit {
             ) === index
         ));
       });
-
-    this.albums = albumData.items.filter(
-      (curValue: any, index: any, self: any) =>
-        self.findIndex(
-          (t: any) => t.name.toUpperCase() === curValue.name.toUpperCase()
-        ) === index
-    );
   }
 
   ngOnDestroy() {
