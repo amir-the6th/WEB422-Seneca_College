d5d1f02c3f6f28b467872efc683bb5ea7243eb99
asabagh <asabagh@myseneca.ca>
2021-12-04 19:49:49 -0500

    Update environment.ts

--- Assignment 6/web422-a6/src/environments/environment.ts
+++ Assignment 6/web422-a6/src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   clientID: '37c1e43a96b3487c93bf49b2d6b5d6f2',
   clientSecret: 'cb029cb734e340329cbef6c1a748655d',
+  userAPIBase: 'https://rocky-everglades-29738.herokuapp.com/api/user',
 };
 
 /*
